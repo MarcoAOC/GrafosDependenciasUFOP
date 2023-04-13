66f94f446ee9ac114e1414161af03f8470164ee1
marco.costa <marco.aoc18@gmail.com>
2023-04-12 21:36:01 -0300

    base change

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  base: '/GrafosDependenciasUFOP/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
